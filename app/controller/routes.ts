a0916809f27c4a7084b794dbdfa90627276f325d
chenxiang <chenxiang1028@xyz.cn>
2018-09-18 16:26:09 +0800

    api

--- app/controller/routes.ts
+++ app/controller/routes.ts
@@ -3,6 +3,7 @@ import { Controller } from 'egg';
 export default class RoutesController extends Controller {
   public async index() {
     // render view template in `client/index`
+    const data = await this.service.user.findAll({ query: '神', limit: 5, start: 0 });
     await this.ctx.render('index', {
       initState: {
         html: this.ctx.helper.getSeo('/'),
--- app/service/user.ts
+++ app/service/user.ts
@@ -1,9 +1,17 @@
 import { Service } from 'egg';
 
+interface SInterface {
+  query: string;
+  start: number;
+  limit: number;
+}
+
 export default class UserService extends Service {
-    public async findAll() {
-      // http://api.zhuishushenqi.com/book/fuzzy-search?query=${query}&start=0&limit=10
-        this.logger.info('Fetch data');
-        return Promise.resolve([ 'Jim', 'Peng', 'Gray' ]);
+    public async findAll(search: SInterface) {
+      this.logger.info('Fetch data');
+      this.ctx.curl('http://api.zhuishushenqi.com/book/fuzzy-search', {
+        data: search,
+      });
+      return Promise.resolve([ 'Jim', 'Peng', 'Gray' ]);
     }
 }
