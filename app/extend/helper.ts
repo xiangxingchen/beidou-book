05a2d7e42dc34596daeaeb771594c0a5464fa700
陈祥 <1447388624@qq.com>
2018-09-18 16:02:33 +0800

    api

--- app/extend/helper.ts
+++ app/extend/helper.ts
@@ -16,10 +16,9 @@ module.exports = {
   getSeo(url: string): object {
     const seo = {
       '/': {
-        title: '新一站保险网触屏版_网上投保，网上买保险，在线投保，保险网',
-        desc: '新一站保险网是国内领先的网络保险电子商务平台，新一站保险网全面支持网上在线投保！网上买保险首选新一站！涵盖国内数十家知名保险公司的保险产品，' +
-        '为您提供从保险产品的咨询、购买到理赔、保全等全过程的一站式服务。',
-        keywords: '网上投保,网上买保险,在线投保,保险网',
+        title: 'beidou-one-title',
+        desc: 'beidou-one-desc',
+        keywords: 'beidou-one-keywords',
       },
       '/ui': {
         title: 'ui',
--- app/service/user.ts
+++ app/service/user.ts
@@ -2,6 +2,7 @@ import { Service } from 'egg';
 
 export default class UserService extends Service {
     public async findAll() {
+      // http://api.zhuishushenqi.com/book/fuzzy-search?query=${query}&start=0&limit=10
         this.logger.info('Fetch data');
         return Promise.resolve([ 'Jim', 'Peng', 'Gray' ]);
     }
